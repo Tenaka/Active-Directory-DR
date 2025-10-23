77ad9381ef831f115c49b5b7003a02eb9ff297b6
Tenaka <86342641+Tenaka@users.noreply.github.com>
2025-10-23 12:30:53 +0100

    Add files via upload

---
+++ List AD Partitions.ps1
@@ -0,0 +1,15 @@
+﻿$hh=@() ; $hh += "-" * 80
+$root = Get-ADRootDSE
+Write-Host $hh
+Write-Host "Active Directory Partitions:" -ForegroundColor Cyan
+Write-Host $hh
+# Domain Partition
+Write-Host "Domain Partition: $($root.defaultNamingContext)"
+# Configuration Partition
+Write-Host "Configuration Partition: $($root.configurationNamingContext)"
+# Schema Partition
+Write-Host "Schema Partition: $($root.schemaNamingContext)"
+# Domain DNS Zones Partition
+Write-Host "DomainDNSZones Partition: DC=DomainDNSZones,$($root.defaultNamingContext)"
+# Forest DNS Zones Partition
+Write-Host "ForestDNSZones Partition: DC=ForestDNSZones,$($root.rootDomainNamingContext)"
